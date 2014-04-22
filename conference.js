fe26bc547534981299bfd4753dc5c0bf321645cf
Pierre Frisch <noreply>
2014-04-22 22:25:50 +0000

    Add module conference.js

---
+++ conference.js
@@ -0,0 +1,16 @@
+/**
+ * @module //conference
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Conference
+ * @extends Montage
+ */
+exports.Conference = Montage.specialize(/** @lends Conference# */ {
+    constructor: {
+        value: function Conference() {
+            this.super();
+        }
+    }
+});
