339b6d6e64f003844247fcfb9e2a03c288173dca
Pierre Frisch <noreply>
2014-04-22 15:50:29 +0000

    Add component chat

---
+++ ui/chat.reel/chat.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/chat.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Chat
+ * @extends Component
+ */
+exports.Chat = Component.specialize(/** @lends Chat# */ {
+    constructor: {
+        value: function Chat() {
+            this.super();
+        }
+    }
+});
